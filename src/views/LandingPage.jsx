c8152c8d945ac375c99c52e526b2f0ab15c9a41c
AlbertLeipzig <albertclemvill@gmail.com>
2023-02-02 19:55:29 +0100

    fetched eventData in the landing page

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,6 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const LandingPage = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/v1/events')
+      .then((res) => {
+        const data = res.data;
+        setEvents(data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
   return (
     <div className="landing-page">
       <div className="landing-page__hero">
